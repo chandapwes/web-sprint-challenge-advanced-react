f9fd24d7d84073c343bb7fcf23671433020cac71
chandapwes <67353485+chandapwes@users.noreply.github.com>
2021-06-04 04:25:56 -0600

    Plants displaying, form created and implimented, tests written and passing

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,28 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from "@testing-library/user-event";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+    render(<CheckoutForm />);
 
-test("form shows success message on submit with form details", () => {});
+    const header = screen.queryByText(/checkout form/i);
+
+    expect(header).toBeInTheDocument();
+    expect(header).toBeTruthy();
+    expect(header).toHaveTextContent(/checkout form/i)
+});
+
+test("form shows success message on submit with form details", () => {
+    render(<CheckoutForm />);
+    
+    const button = screen.getByRole('button');
+    userEvent.click(button);
+
+    const submit = screen.getByTestId('successMessage');
+    expect(submit).toBeInTheDocument();
+
+  
+});
